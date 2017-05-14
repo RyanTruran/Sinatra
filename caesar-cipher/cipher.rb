ea2da818f1a8124db0abbaf6c15ce09532d6b36d
Ryan Truran <ryan.truran@gmail.com>
2017-05-14 17:12:14 -0600

    added caesar-cipher website

---
+++ caesar-cipher/cipher.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+cipher_string = ""
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  string = params["string"]
+  shift = params["shift"].to_i
+  cipher_string = ceaser_cipher(string,shift)
+  erb :ciphered, :locals => {:cipher_string => cipher_string}
+end
+
+def ceaser_cipher(string, shift=3)
+  #take a string and copy to an array;
+  chars=string.upcase.split("")
+  conv=[]
+  chars.each do |char|
+    if char.match(/[[:alnum:]]/)
+      asc = char.ord
+      for i in 1..shift do
+        asc=asc-1
+        if asc===64
+          asc=90
+        end
+      end
+      conv.push(asc.chr)
+    else
+      conv.push(char)
+    end
+  end
+  return conv.join("")
+end
+
+def Check_Guess(guess)
+  if guess>$random_number
+    return "Your guess was too high"
+  elsif guess<$random_number
+    return "Your guess was too low"
+  elsif guess==$random_number
+    return "You got it"
+  end
+end
--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number=rand(100)
-
+$random_number=rand(100)
+guess_string = ""
 get '/' do
-  erb :index, :locals => {:random_number => random_number}
+  guess = params["guess"].to_i
+  guess_string = Check_Guess(guess)
+  erb :index, :locals => {:random_number => $random_number, :guess_string => guess_string}
+end
+
+def Check_Guess(guess)
+  if guess>$random_number
+    return "Your guess was too high"
+  elsif guess<$random_number
+    return "Your guess was too low"
+  elsif guess==$random_number
+    return "You got it"
+  end
 end
